fb5efd4b754840cefba332446383487069dd5b99
Leo Ermebring <leo.ermebring@edu.lycksele.se>
2025-01-10 13:46:55 +0100

    first commit

---
+++ Skriva till fil/Program.cs
@@ -0,0 +1,5 @@
+﻿FileStream fs =new FileStream(".txt",FileMode.Open,FileAccess.Read);
+StreamReader sr = new StreamReader(fs);
+string tjena = sr.ReadToEnd();
+sr.Close();
+Console.WriteLine(tjena);
